0ca36e881d9904d89d878bca94014319bcabf8af
Juan <jm.munoz14@uniandes.edu.co>
2019-03-03 18:57:17 -0500

    terminados usuario, preguntas y respuestas

--- routes/preguntas.js
+++ routes/preguntas.js
@@ -5,5 +5,6 @@ const ctrlPregunta = require('../controllers/preguntas');
  
 router.post('/newquestion', ctrlPregunta.register);
 router.get('/obtainquestion', ctrlPregunta.obtain);
- 
+router.get('/allquestion', ctrlPregunta.obtainall);
+router.delete('/delquest', ctrlPregunta.remove);
 module.exports = router;

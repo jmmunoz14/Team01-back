725fea2e31192149008fd3367a671528bcfe2bfc
Juan <jm.munoz14@uniandes.edu.co>
2019-03-03 19:18:11 -0500

    ya deja de joderme el proyecto git de mierda

--- routes/respuestas.js
+++ routes/respuestas.js
@@ -5,6 +5,6 @@ const ctrlRespuesta = require('../controllers/respuestas');
  
 router.post('/newanswer', ctrlRespuesta.register);
 router.get('/obtainanswer', ctrlRespuesta.obtain);
-
- 
+router.get('/allanswer', ctrlRespuesta.obtainall);
+router.delete('/delanswer', ctrlRespuesta.remove);
 module.exports = router;

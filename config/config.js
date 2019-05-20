b3adce0165eccd25de29b8a6513e5372c2bad39a
Juan <jm.munoz14@uniandes.edu.co>
2019-05-20 05:17:40 -0500

    sdad

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 
 // check env.
-var env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || 'production';
 // fetch env. config
 var config = require('./config.json');
 var envConfig = config[env];
